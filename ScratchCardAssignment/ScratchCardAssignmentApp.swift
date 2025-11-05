5704c0e260f0ace35756f16fbad05c0da5faa8ec
Stanislav Révay <stanislavrevay96@gmail.com>
2025-11-05 16:13:57 +0100

    Initial Commit

---
+++ ScratchCardAssignment/ScratchCardAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScratchCardAssignmentApp.swift
+//  ScratchCardAssignment
+//
+//  Created by Stanislav Révay on 05/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ScratchCardAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
